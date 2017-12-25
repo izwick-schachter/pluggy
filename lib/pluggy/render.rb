40b49811218f0bb3069b891d4843c4613e6ee1cc
izwick-schachter <isaiah@developingtechnician.com>
2017-12-25 17:15:03 -0500

    Cleaned up View. Added deprecation notice for @metadata.

--- lib/pluggy/render.rb
+++ lib/pluggy/render.rb
@@ -1,14 +1,20 @@
 module Pluggy
   class View
-    attr_reader :content, :metadata
+    attr_reader :content, :mime_type, :filename
     
     def initialize(content = nil, mime_type: nil, filename: '')
       content = content.content while content.is_a?(View)
       @content = content
-      filename = File.basename filename
-      @metadata = {
-        mime_type: mime_type || (filename.empty? ? 'text/html' : Rack::Mime.mime_type(".#{filename.split('.')[1]}")),
-        filename: filename
+      @filename = File.basename filename
+      ext = @filename.split('.')[1]
+      @mime_type = mime_type || (@filename.empty? ? 'text/html' : Rack::Mime.mime_type(".#{ext}"))
+    end
+
+    def metadata
+      warn "Using View#metadata is deprecated. Please use either View#mime_type or View#filename"
+      {
+        mime_type: @mime_type,
+        filename: @filename
       }
     end
   end
